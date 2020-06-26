8794f0fea76bfb20c16ddfc2ed0ee4c70b317e45
lethosor <lethosor@gmail.com>
2020-06-25 23:13:39 -0400

    Add initial codegen.py - generates codegen.out.xml only
    Note that some changes are made to elements by codegen.pl (e.g. adding `last-item` and `anon-name` attributes), which this script does not yet handle

---
+++ codegen.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import argparse
+import glob
+import os
+import sys
+
+import lxml.etree
+
+
+parser = argparse.ArgumentParser(description='List all generated files')
+parser.add_argument('input_dir', nargs='?', default='.')
+parser.add_argument('output_dir', nargs='?', default='codegen')
+parser.add_argument('separator', nargs='?', default='\n')
+
+
+xslt_transforms = [lxml.etree.XSLT(lxml.etree.parse('lower-%i.xslt' % i)) for i in (1, 2)]
+
+
+def load_and_transform(filename):
+    tree = lxml.etree.parse(filename)
+    for transform in xslt_transforms:
+        tree = transform(tree)
+    return tree
+
+
+def write_codegen_out_xml(trees, output_dir):
+    src_root = trees[0].getroot()
+    new_root = src_root.makeelement('{%s}data-definition' % src_root.nsmap['ld'], nsmap=src_root.nsmap)
+    new_root.text = '\n    '
+    for tree in trees:
+        for child in tree.getroot().iterchildren():
+            new_root.append(child)
+    with open(os.path.join(output_dir, 'codegen.out.xml'), 'wb') as f:
+        f.write(lxml.etree.tostring(new_root))
+
+
+def main():
+    args = parser.parse_args()
+    os.makedirs(args.output_dir, exist_ok=True)
+    filenames = glob.glob(os.path.join(args.input_dir, "df.*.xml"))
+    filenames.sort()
+    trees = list(map(load_and_transform, filenames))
+    write_codegen_out_xml(trees, args.output_dir)
+
+
+if __name__ == '__main__':
+    main()
